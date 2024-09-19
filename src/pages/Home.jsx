2581eefc21201dbc48e8355daf3991cf355b8100
Shahzaib Balouch <muhammad.shahzaib@arwatechnologies.com>
2024-09-20 01:08:57 +0500

    after add the complete services and set the cursor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,43 @@ export default function Home() {
 
   const data = [
     {
-      heading: 'Trans ',
-      description: 'fgkjfdh jldfsh ',
+      heading: 'Transportation and Chauffeur Service ',
+      description: 'zana Luxury Service offers premier chauffeur-driven transportation, blending elegance and efficiency. Our professional drivers and high-end vehicles ensure a sophisticated, seamless, and personalized journey for our esteemed clients.',
       image: 'assets/img/services.jpg'
     },
     {
-      heading: 'Trans ',
-      description: 'fgkjfdh jldfsh ',
+      heading: 'Our experience ',
+      description: 'Rzana Luxury Service Since 2015, Rzana Luxury Service has been delivering exceptional transportation and golf car services with chauffeur, blending elegance and efficiency to offer a superior, seamless travel experience...',
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'Our vision',
+      description: 'our vision is to always innovate new and modern strategies and services for our customers in everything that is offered through the company to reach their complete satisfaction in every step of their journey with us.',
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'topic Hospitality',
+      description: "At Rzana Luxury Service, we deliver exceptional hospitality, offering personalized, high-end experiences that cater to every client's unique needs with unparalleled attention to detail.",
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'Catering',
+      description: "Rzana offers top-quality catering with a diverse range of food, including exquisite Saudi cuisine, ensuring a premium dining experience tailored to any occasion.",
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'Valet parking',
+      description: "Since 2015, Rzana has been Saudi Arabia's first valet service and pioneer in using advanced technology, setting a new standard in luxury and efficiency.",
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'Parking management',
+      description: "Rzana provides advanced parking management with smart gate technology, ensuring efficient, secure, and seamless parking solutions for an enhanced customer experience.",
+      image: 'assets/img/services.jpg'
+    },
+    {
+      heading: 'UMRAH & HOLY SITES SERVICE',
+      description: "Rzana Luxury Service enhances Umrah and Hajj experiences with premium chauffeur and golf car services, offering seamless transportation, expert hospitality, and efficient parking management at holy sites, ensuring a comfortable, respectful, and stress-free pilgrimage journey. REACH YOUR DESTINATION",
       image: 'assets/img/services.jpg'
     },
   ]
@@ -40,14 +70,14 @@ export default function Home() {
     default: {
       x: mousePosition.x - 8,
       y: mousePosition.y - 8,
-      height: 10,
-      width: 10,
+      height: 5,
+      width: 5,
     },
     hover: {
-      height: 64,
-      width: 64,
-      x: mousePosition.x - 32,
-      y: mousePosition.y - 32,
+      height: 16,
+      width: 16,
+      x: mousePosition.x - 5,
+      y: mousePosition.y - 5,
       backgroundColor: 'transparent',
       mixBlendMode: 'difference',
     },
@@ -59,7 +89,7 @@ export default function Home() {
   return (
     <div>
       <motion.div
-        className="cursor"
+        className=""
         variants={variants}
         animate={cursorVariant}
         style={{
